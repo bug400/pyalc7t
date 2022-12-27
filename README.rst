a53227fadfddac92aa174933150cc6301daec5b5
bug400 <bug400@gmx.de>
2022-12-27 10:14:52 +0100

    Updated LICENSE, prepare PyPI installation

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 pyALC7T
 =======================
-This python application provides a data logger for the
-ELV ALC7000 charger. This is a python version of the
-Basic program written by Frank Steinberg.
+This python application provides a control program and data logger for the
+ELV ALC7000 charger. This is a python version of the Basic program written by 
+Frank Steinberg.
--- setup.py
+++ setup.py
@@ -36,10 +36,10 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Modelcrafters',
-        'Topic :: Software Development :: Tools',
+        'Topic :: Utilities',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     # What does your project relate to?
